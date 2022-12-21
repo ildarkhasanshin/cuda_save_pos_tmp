5b02571b62a504efe442eadfb01d33ee8ec32244
ildar r. khasanshin <ildar.khasanshin@gmail.com>
2022-12-21 23:05:06 +0500

    [+] save bookmarks

--- __init__.py
+++ __init__.py
@@ -4,13 +4,13 @@ from pathlib import Path
 import tempfile
 import json
 
+
 class Command:
     def __init__(self):
         settings_dir = Path(app_path(APP_DIR_SETTINGS))
-        self.history_filename = settings_dir / "cuda_pos_tmp.json"
+        self.history_filename = settings_dir / 'cuda_pos_tmp.json'
 
     def check_tmp_dir(self, path):
-        temp_dir = tempfile.TemporaryDirectory()
         if path.startswith(tempfile.gettempdir()):
             return True
 
@@ -29,6 +29,14 @@ class Command:
         ed_self.set_caret(0, line)
         ed_self.set_prop(PROP_LINE_TOP, max(0, line-3))
 
+    def get_bookmarks(self, ed_self):
+        return ed_self.bookmark(BOOKMARK_GET_LIST, 0)
+
+    def set_bookmarks(self, ed_self, bookmarks_):
+        if (isinstance(bookmarks_, list)):
+            for bookmark_ in bookmarks_:
+                ed_self.bookmark(BOOKMARK_SET, bookmark_)
+
     def on_close_pre(self, ed_self):
         path_full = ed_self.get_filename()
         if (self.check_tmp_dir(path_full)):
@@ -38,26 +46,34 @@ class Command:
     def on_open(self, ed_self):
         path_full = ed_self.get_filename()
         data_load = self.load_pos()
+        bookmarks_ = []
         for path, line in data_load.items():
             if path_full.endswith(path):
-                self.set_caret(ed_self, line)
+                if (isinstance(line, int)):
+                    line_ = line
+                elif (isinstance(line, list)):
+                    line_ = line[0]
+                    bookmarks_ = line[1]
+                    self.set_bookmarks(ed_self, bookmarks_)
+                self.set_caret(ed_self, line_)
 
     def load_pos(self):
         data_load = {}
         if self.history_filename.exists():
-            with self.history_filename.open(encoding="utf-8") as fin:
+            with self.history_filename.open(encoding='utf-8') as fin:
                 data_load = json.load(fin)
 
         return data_load
 
     def save_pos(self, path, ed_self):
         if self.get_caret(ed_self) > 0:
-            data = {path: self.get_caret(ed_self)}
+            data_ = [self.get_caret(ed_self), self.get_bookmarks(ed_self)]
+            data = {path: data_}
             data_load = self.load_pos()
             if data_load:
-                data_load[path] = self.get_caret(ed_self)
+                data_load[path] = data_
             else:
                 data_load.update(data)
 
-            with self.history_filename.open(mode="w", encoding="utf-8") as fout:
+            with self.history_filename.open(mode='w', encoding='utf-8') as fout:
                 json.dump(data_load, fout, indent=2)
